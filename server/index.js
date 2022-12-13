62807f9f74cb7c26baa3f935675c492d6a4c3c4c
humble-codes <sprudra5001@outlook.com>
2022-12-14 02:56:08 +0530

    cors-change in indexjs

--- server/index.js
+++ server/index.js
@@ -6,13 +6,13 @@ const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
-
+const corsOptions = require('./config/corsOptions')
 const app = express();
 
 // Connect to database
 connectDB();
 
-app.use(cors());
+app.use(cors(corsOptions)) ;
 
 app.use(
   '/graphql',
